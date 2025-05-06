053316862e6322c9d3d8c9df683071f43e049d4c
Abdelrhman <abdo612200320@gmail.com>
2025-05-06 21:42:51 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+function App() {
+  return (
+    <section>
+      <div className="container">
+        <div className="top_sec">
+          <div className="city">
+            <h3>المدينه</h3>
+
+            <select name="" id="">
+              <option value={20}> القاهره</option>
+              <option value={20}> الجيزه</option>
+            </select>
+          </div>
+
+          <div className="date">
+            <h3>التاريخ</h3>
+            <h2>5/6/2025</h2>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default App;
