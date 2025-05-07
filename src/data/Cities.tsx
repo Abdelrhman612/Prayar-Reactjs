98ae6a5b1e1e45dee56e0c73e41d961720d6e56d
Abdelrhman <abdo612200320@gmail.com>
2025-05-07 12:31:24 +0300

    feat: create Cities.tsx

---
+++ src/data/Cities.tsx
@@ -0,0 +1,29 @@
+export const cities = [
+  {
+    id: 1,
+    name: "القاهره",
+    value: "Cairo",
+  },
+  {
+    id: 2,
+
+    name: "الاسكندريه",
+    value: "Alexandria",
+  },
+  {
+    id: 3,
+
+    name: "المنصوره",
+    value: "Mansoura",
+  },
+  {
+    id: 4,
+    name: "اسوان",
+    value: "Aswan",
+  },
+  {
+    id: 5,
+    name: "الاقصر",
+    value: "luxor",
+  },
+];
